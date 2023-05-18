ba98beb0ae1dff10038244ef1b40720c92de4af3
Daniel Istvan Nemeth <nemeth.daniel.istvan@hallgato.ppke.hu>
2023-05-18 22:19:34 +0200

    Bugfix, matching algo

--- app/src/main/java/hu/awesometeam/keyper/PasswordVault.java
+++ app/src/main/java/hu/awesometeam/keyper/PasswordVault.java
@@ -35,10 +35,14 @@ public class PasswordVault {
         if(uriString.equals("")) throw new FileNotFoundException("Empty path");
         if(pass.equals("")) throw new FileNotFoundException("Empty password");
 
-        String path = Environment.getExternalStorageDirectory() + File.separator + Uri.decode(uriString).split("primary:")[1];
+        String path = Environment.getExternalStorageDirectory() + File.separator + "keyper.kdbx";//Uri.decode(uriString).split("primary:")[1];
         File f = new File(path);
 
-        load(f, pass);
+        try {
+            load(f, pass);
+        }catch (de.slackspace.openkeepass.exception.KeePassDatabaseUnreadableException e){
+            Toast.makeText(ctx, "Database unreadable", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public static PasswordVault getInstance(Context context){
@@ -58,12 +62,21 @@ public class PasswordVault {
     private void load(File f, String password) {
         db = KeePassDatabase.getInstance(f).openDatabase(password);
     }
-    public String checkIfMatch(String input) {
+    public String[] checkIfMatch(String input) {
         // Retrieve all entries
         List<Entry> entries = db.getEntries();
         for (Entry entry : entries) {
-            if ((entry.getPassword() != null && entry.getPassword().length() > 0 && input.contains(entry.getPassword()) || (entry.getCustomProperties() != null && entry.getCustomProperties().stream().anyMatch(p -> p != null && p.getPropertyValue().getValue().length() > 0 && input.contains(p.getPropertyValue().getValue()))))) {
-                return entry.getTitle();
+            if ((entry.getPassword() != null && entry.getPassword().length() >= 3 && input.contains(entry.getPassword()) || (entry.getCustomProperties() != null && entry.getCustomProperties().stream().anyMatch(p -> p != null && p.getPropertyValue().getValue().length() >= 3 && input.contains(p.getPropertyValue().getValue()))))) {
+                if (entry.getUrl() != null && entry.getUrl().length() > 0) {
+                    String[] parts = new String[2];
+                    parts[0] = entry.getTitle();
+                    parts[1] = entry.getUrl();
+                } else {
+                    String[] parts = new String[1];
+                    parts[0] = entry.getTitle();
+                    return parts;
+                }
+
             }
         }
 
