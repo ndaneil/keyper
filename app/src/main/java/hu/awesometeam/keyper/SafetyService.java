d835a1d974731c4440cc1e7e66b0a21525aec065
StockVakond <levente.bolyki@gmail.com>
2023-05-18 20:06:37 +0200

    service init- works based on logs

---
+++ app/src/main/java/hu/awesometeam/keyper/SafetyService.java
@@ -0,0 +1,95 @@
+package hu.awesometeam.keyper;
+
+import android.accessibilityservice.AccessibilityService;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SafetyService extends AccessibilityService {
+    private String lastUrl = "";
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        ///TODO init
+        Log.i("AccService", "onServiceConnected");
+    }
+
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED ||
+                event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+            if (rootNode != null) {
+                String currentUrl = getCurrentUrl(rootNode);
+                if(!currentUrl.equals("")){
+                    lastUrl = currentUrl;
+                    Log.d("AccService", "lastUrl: " + lastUrl);
+                }
+            }
+        }
+        if(event.getEventType()== AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED){
+            AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+            Log.d("AccService", "TypedText: "+event.getText().toString());
+        }
+
+    }
+
+    @Override
+    public void onInterrupt() {
+        // Handle accessibility interruption if needed
+    }
+
+    private String getCurrentUrl(AccessibilityNodeInfo rootNode) {
+        AccessibilityNodeInfo addressBarNode = findAddressBarNode(rootNode);
+        if (addressBarNode != null && addressBarNode.getText() != null) {
+            return addressBarNode.getText().toString();
+        }
+        return "";
+    }
+    private AccessibilityNodeInfo findAddressBarNode(AccessibilityNodeInfo rootNode) {
+        if (rootNode == null) {
+            return null;
+        }
+        List<AccessibilityNodeInfo> addressBars = null;
+        if (rootNode.getPackageName() == null) {
+            return null;
+        }
+        if(Objects.equals((String) rootNode.getPackageName(), "com.android.chrome")){
+            ///TODO find on seperate thread maybe
+            addressBars = rootNode.findAccessibilityNodeInfosByViewId("com.android.chrome:id/url_bar");
+        }
+        if (addressBars == null || addressBars.isEmpty()) {
+            return null;
+        }
+        return addressBars.get(0);
+    }
+    public static void logViewHierarchy(AccessibilityNodeInfo nodeInfo, final int depth) {
+
+        if (nodeInfo == null) return;
+
+        String spacerString = "";
+
+        for (int i = 0; i < depth; ++i) {
+            spacerString += '-';
+        }
+        //Log the info you care about here... I choce classname and view resource name, because they are simple, but interesting.
+        if(nodeInfo.getClassName().equals("android.widget.EditText")) {
+            Log.d("TAG", spacerString + nodeInfo.getClassName() + " " + nodeInfo.getViewIdResourceName());
+            Log.d("TAG", " " +spacerString + nodeInfo.getClassName() +nodeInfo.getText());
+            Log.d("TAG", " " +spacerString + nodeInfo.getClassName() +nodeInfo.findFocus(AccessibilityNodeInfo.FOCUS_INPUT));
+
+
+        }
+        for (int i = 0; i < nodeInfo.getChildCount(); ++i) {
+            logViewHierarchy(nodeInfo.getChild(i), depth+1);
+        }
+    }
+
+
+
+}
+
