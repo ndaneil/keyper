4acd6501e856fd1f6cfbd9e0a8503dcd04abc470
StockVakond <levente.bolyki@gmail.com>
2023-05-18 23:15:16 +0200

    overlay_perm

--- app/src/main/java/hu/awesometeam/keyper/SettingsActivity.java
+++ app/src/main/java/hu/awesometeam/keyper/SettingsActivity.java
@@ -10,6 +10,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -24,6 +25,7 @@ import androidx.preference.PreferenceManager;
 
 
 public class SettingsActivity extends AppCompatActivity {
+    private static final int OVERLAY_PERM_CODE = 101;
     private final ActivityResultLauncher<String[]> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), isGranted -> {
                 if (isGranted.values().stream().allMatch(e-> e)) {
@@ -50,7 +52,6 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
         findViewById(R.id.tvCheckNotif).setOnClickListener(view ->
                 startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS)));
         findViewById(R.id.tvCheckSave).setOnClickListener(view -> {
@@ -73,6 +74,16 @@ public class SettingsActivity extends AppCompatActivity {
                 PERMISSIONS = new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
                 Log.i("permission", " trying the old way");
                 requestPermissionLauncher.launch(PERMISSIONS);
+            }
+        });
+
+        findViewById(R.id.tvCheckOverlay).setOnClickListener(view -> {
+            if (!Settings.canDrawOverlays(getApplicationContext())){
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getApplicationContext().getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERM_CODE);
+            }else{
+                Toast.makeText(getApplicationContext(),"Overlay permission already granted",Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -135,6 +146,11 @@ public class SettingsActivity extends AppCompatActivity {
                     } else {
                         status.setTitle("DB load succeeded");
                     }
+                }
+            }
+            else if (requestCode == OVERLAY_PERM_CODE) {
+                if (!Settings.canDrawOverlays(getActivity().getApplicationContext())) {
+                    Toast.makeText(getActivity(), "Permission not granted", Toast.LENGTH_SHORT).show();
                 }
             }
             super.onActivityResult(requestCode, resultCode, resultData);
